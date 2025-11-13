5a536f0c458e0c75ed3c9e391930a781dfd4d148
wahyudi.indra <wahyudiindra79@gmail.com>
2025-11-13 16:16:17 +0700

    feat(exception): handling error using exception filter

--- prisma/seeders/admin.seeder.ts
+++ prisma/seeders/admin.seeder.ts
@@ -2,7 +2,7 @@ import { Prisma } from './prisma.utils';
 import { Prisma as PrismaClient } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
 import { fakerID_ID } from '@faker-js/faker';
-import { DEFAULT_ADMIN_EMAIL, DEFAULT_PASSWORD } from './../../src/helpers/constants/default';
+import { DEFAULT_ADMIN_EMAIL, DEFAULT_PASSWORD } from '../../src/common/constants/default';
 
 export const run = async (total: number) => {
     const encryptedPass = await bcrypt.hash(DEFAULT_PASSWORD, 10);
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ExceptionsFilter } from './common/filters/exceptions.filter';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(
         new ValidationPipe({ whitelist: true, transform: true, transformOptions: { enableImplicitConversion: true } }),
     );
+
+    app.useGlobalFilters(new ExceptionsFilter());
 
     const options = new DocumentBuilder()
         .setTitle('Activity Tracker API')
