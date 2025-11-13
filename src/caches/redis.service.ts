151cc24b86147a7247e727214ebfae775f04c972
wahyudi.indra <wahyudiindra79@gmail.com>
2025-11-14 06:37:23 +0700

    fix(cache): implement onModuleInit and retryStratefy on redisService

--- src/caches/redis.service.ts
+++ src/caches/redis.service.ts
@@ -1,23 +1,33 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import Redis from 'ioredis';
 
 @Injectable()
-export class RedisService {
+export class RedisService implements OnModuleInit {
     private readonly logger = new Logger(RedisService.name);
 
     public client: Redis;
     public isDown = false;
 
     constructor() {
-        this.client = new Redis(Number(process.env.REDIS_PORT), String(process.env.REDIS_HOST));
+        this.client = new Redis({
+            port: Number(process.env.REDIS_PORT),
+            host: String(process.env.REDIS_HOST),
+            retryStrategy(times) {
+                return Math.min(times * 200, 1000 * 2);
+            },
+            maxRetriesPerRequest: 0,
+            connectTimeout: 2000,
+            enableReadyCheck: true,
+        });
 
-        this.client.on('connect', () => {
-            this.logger.log('Redis connected');
+        this.client.on('ready', () => {
+            this.logger.log('Redis ready');
             this.isDown = false;
         });
 
         this.client.on('error', (err) => {
-            this.logger.error(`Redis error: ${err.message}`);
+            const message = err?.message || err?.['code'] || err?.['errno'] || err?.['syscall'] || JSON.stringify(err);
+            if (!this.isDown) this.logger.error(`Redis error: ${message}`);
             this.isDown = true;
         });
 
@@ -27,12 +37,23 @@ export class RedisService {
         });
     }
 
+    async onModuleInit() {
+        try {
+            await this.client.ping();
+            this.isDown = false;
+        } catch {
+            this.logger.error('Redis is DOWN at startup');
+            this.isDown = true;
+        }
+    }
+
     async safeGet(key: string): Promise<string | null> {
         if (this.isDown) return null;
 
         try {
             return await this.client.get(key);
         } catch (e) {
+            this.logger.error(`safeGet failed: ${e.message}`);
             this.isDown = true;
             return null;
         }
